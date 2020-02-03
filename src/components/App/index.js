e6a7183328070838bbf4e32ed028daee236c9ac5
Derrick Sekidde <kidricederek@gmail.com>
2020-02-03 14:11:33 +0300

    Add styling

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import './App.css';
 import Footer from '../Footer/';
 import ClusterResources from '../ClusterResources/';
+import ResourceList from '../ResourceList/';
 
 /* These below are for showing purposes */
 /* THIS IS GOING TO BE REMOVED IN THE DASHBOARD FEATURE */
 
 function App() {
   return <div>
-    <div className="Disposable">
-      <div><ClusterResources /></div>
-    </div>
+    {/* <div className="Disposable"> */}
+      {/* <div><ClusterResources /></div> */}
+    {/* </div> */}
+    <ResourceList />
     <Footer />
   </div>
 }
