6416d37c5c287a7676a973a2d788bc2d0ecc5c63
Derrick Sekidde <kidricederek@gmail.com>
2020-02-04 13:43:02 +0300

    Add css to component

--- src/components/ResourceList/index.js
+++ src/components/ResourceList/index.js
@@ -1,40 +1,48 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import './ResourceList.css';
 
 const namespaces = [
     {
       "name": "Cranecloud-m1",
       "resourceNumber": 7,
+      "status": "running",
       "id": 1
     },
     {
       "name": "Mak-w3",
       "resourceNumber": 3,
+      "status": "running",
       "id": 2
     },
     {
       "name": "AirQuality",
       "resourceNumber": 11,
+      "status": "running",
       "id": 3
     },
     {
       "name": "Cocis",
       "resourceNumber": 9,
+      "status": "paused",
       "id": 4
     },
     {
       "name": "Level5Hub",
       "resourceNumber": 5,
+      "status": "running",
       "id": 5
     },
     {
       "name": "Catalysts",
       "resourceNumber": 0,
+      "status": "offline",
       "id": 6
     },
     {
       "name": "Backup",
       "resourceNumber": 2,
+      "status": "stopped",
       "id": 7
     }
   ]
@@ -56,30 +64,30 @@ const namespaces = [
           <div className="Entire-List">
             <div id="blank"></div>
             <div class="All-Resources">
-                <table>
-                  <thead>
-                    <tr>
-                      <th>Name</th>
-                      <th>Status</th>
-                      <th>View</th>
-                      <th>Delete</th>
-                    </tr>
-                  </thead>
-                    <tbody id="Listing">
-                    {
-                      namespaces.map((obj) => {
-                          return <div>
-                            <tr>
-                              <td class="Resource-Name"> {obj.name} </td>
-                              <td class="Resource-Status"> {obj.status} </td>
-                              <td><a href ="node/{obj.id}">View</a></td>
-                              <td><a href ="node/delete/{msg.message_id}">Delete</a></td>
-                            </tr>
-                          </div>
-                      })
-                      }
-                    </tbody>
-                </table>
+              <table className="My-Table">
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Status</th>
+                    <th>View</th>
+                    <th>Delete</th>
+                  </tr>
+                </thead>
+                  <tbody id="Listing">
+                  {
+                    namespaces.map((obj) => {
+                        return <div>
+                          <tr>
+                            <td class="Resource-Name"> {obj.name} </td>
+                            <td class="Resource-Status"> {obj.status} </td>
+                            <td><a href ="node/{obj.id}">View</a></td>
+                            <td><a href ="node/delete/{msg.message_id}">Delete</a></td>
+                          </tr>
+                        </div>
+                    })
+                    }
+                  </tbody>
+              </table>
             </div>
           </div>
         </section>
